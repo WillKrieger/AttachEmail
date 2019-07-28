ac8b60ea437e784a491ad746ab91ecfbef6016d7
Guillermo Guerrero Jaime <willkrieger@MacBook-Pro-de-Guillermo.local>
2019-07-27 21:28:08 -0500

    first commit

---
+++ EnvioCorreo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EnvioCorreo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utilidades.Correo mail = new Utilidades.Correo();
+
+            mail.CorreoRemitente = "guijac2000@gmail.com";
+            mail.CorreoDestinatario = "gui_jac2000@hotmail.com";
+            mail.Contrasena = "WillStyle";
+            mail.NombreRemitente = "Will Krieger";
+            mail.Asunto = "Asunto";
+            mail.Mensaje = "Mensaje de prueba";
+            mail.ArchivoAdjunto = System.IO.Path.GetFullPath(@"/Users/willkrieger/Projects/EnvioCorreo/EnvioCorreo/bin/Debug/netcoreapp2.2/EnvioCorreo.deps.json");
+
+            mail.ServidorSmtp = "smtp.gmail.com";
+            mail.Puerto = 587;
+            mail.SSL = true;
+
+            mail.Enviar();
+        }
+    }
+}
---
+++ EnvioCorreo/Utilidades/Correo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace EnvioCorreo.Utilidades
+{
+    public class Correo
+    {
+        public string ServidorSmtp { get; set; }
+        public int Puerto { get; set; }
+        public bool SSL { get; set; }
+        public string CorreoRemitente { get; set; }
+        public string NombreRemitente { get; set; }
+        public string CorreoDestinatario { get; set; }
+        public string Asunto { get; set; }
+        public string Mensaje { get; set; }
+        public string Contrasena { get; set; }
+        public string ArchivoAdjunto { get; set; }
+
+        public void Enviar()
+        {
+            try
+            {
+                MailMessage correo = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+
+                correo.From = new MailAddress(CorreoRemitente, NombreRemitente, System.Text.Encoding.UTF8);
+                correo.To.Add(CorreoDestinatario);
+
+                correo.Subject = Asunto;
+                correo.SubjectEncoding = System.Text.Encoding.UTF8;
+
+                correo.Body = Mensaje;
+                correo.BodyEncoding = System.Text.Encoding.UTF8;
+
+                correo.IsBodyHtml = false;
+                correo.Priority = MailPriority.Normal;
+
+                correo.Attachments.Add(new Attachment(ArchivoAdjunto));
+
+                smtp.Credentials = new System.Net.NetworkCredential(CorreoRemitente, Contrasena);
+                smtp.Port = Puerto;
+                smtp.Host = ServidorSmtp;
+                smtp.EnableSsl = SSL;
+
+                smtp.Send(correo);
+
+
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+    }
+}
